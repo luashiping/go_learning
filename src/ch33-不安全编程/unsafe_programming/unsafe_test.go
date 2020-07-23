ad3baac03d830b8d589554b91db48f7b7c9689e6
luashiping <18069879858@163.com>
2020-07-23 13:38:26 +0800

    add ch33

---
+++ src/ch33-不安全编程/unsafe_programming/unsafe_test.go
@@ -0,0 +1,74 @@
+package unsafe_test
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+	"time"
+	"unsafe"
+)
+
+type Customer struct {
+	Name string
+	Age  int
+}
+
+func TestUnsafe(t *testing.T) {
+	i := 10
+	t.Log(i)
+	t.Log(&i)
+	f := *(*float64)(unsafe.Pointer(&i))
+	t.Log(unsafe.Pointer(&i)) // 指针的值
+	t.Log(f)
+}
+
+// The cases is suitable for unsafe
+type MyInt int
+
+//合理的类型转换
+func TestConvert(t *testing.T) {
+	a := []int{1, 2, 3, 4}
+	t.Log(a)
+	t.Log(&a)
+	b := *(*[]MyInt)(unsafe.Pointer(&a))
+	t.Log(unsafe.Pointer(&a))
+	t.Log(b)
+}
+
+//原子类型操作
+func TestAtomic(t *testing.T) {
+	var shareBufPtr unsafe.Pointer
+	writeDataFn := func() {
+		data := []int{}
+		for i := 0; i < 100; i++ {
+			data = append(data, i)
+		}
+		atomic.StorePointer(&shareBufPtr, unsafe.Pointer(&data))
+	}
+	readDataFn := func() {
+		data := atomic.LoadPointer(&shareBufPtr)
+		fmt.Println(data, *(*[]int)(data))
+	}
+	var wg sync.WaitGroup
+	writeDataFn()
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			for i := 0; i < 10; i++ {
+				writeDataFn()
+				time.Sleep(time.Microsecond * 100)
+			}
+			wg.Done()
+		}()
+		wg.Add(1)
+		go func() {
+			for i := 0; i < 10; i++ {
+				readDataFn()
+				time.Sleep(time.Microsecond * 100)
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+}
