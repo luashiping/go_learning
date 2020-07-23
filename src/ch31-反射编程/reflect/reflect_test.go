cd9f4545fbb917009c843dfd1a05c2dbed66b99d
luashiping <18069879858@163.com>
2020-07-23 09:55:01 +0800

    update ch31

--- src/ch31-反射编程/reflect/reflect_test.go
+++ src/ch31-反射编程/reflect/reflect_test.go
@@ -30,25 +30,6 @@ func TestBasicType(t *testing.T) {
 
 }
 
-func TestDeepEqual(t *testing.T) {
-	a := map[int]string{1: "one", 2: "two", 3: "three"}
-	b := map[int]string{1: "one", 2: "two", 3: "three"}
-	//      t.Log(a == b)
-	t.Log("a==b?", reflect.DeepEqual(a, b))
-
-	s1 := []int{1, 2, 3}
-	s2 := []int{1, 2, 3}
-	s3 := []int{2, 3, 1}
-
-	t.Log("s1 == s2?", reflect.DeepEqual(s1, s2))
-	t.Log("s1 == s3?", reflect.DeepEqual(s1, s3))
-
-	c1 := Customer{"1", "Mike", 40}
-	c2 := Customer{"1", "Mike", 40}
-	fmt.Println(c1 == c2)
-	fmt.Println(reflect.DeepEqual(c1, c2))
-}
-
 type Employee struct {
 	EmployeeID string
 	Name       string `format:"normal"`
@@ -57,12 +38,6 @@ type Employee struct {
 
 func (e *Employee) UpdateAge(newVal int) {
 	e.Age = newVal
-}
-
-type Customer struct {
-	CookieID string
-	Name     string
-	Age      int
 }
 
 func TestInvokeByName(t *testing.T) {
