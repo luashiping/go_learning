6d2f351710e47d533ab41bf96be60ab6f929766f
luashiping <18069879858@163.com>
2020-07-27 16:10:42 +0800

    add ch8

--- src/ch8-Map与工厂模式，在Go语言中实现Set/map/map_ext_test.go
+++ src/ch8-Map与工厂模式，在Go语言中实现Set/map/map_ext_test.go
@@ -3,5 +3,30 @@ package map_ext
 import "testing"
 
 func TestMapWithFunValue(t *testing.T) {
-	m1:=map[int]func(op int)int
+	m := map[int]func(op int) int{}
+	m[1] = func(op int) int { return op }
+	m[2] = func(op int) int { return op * op }
+	m[3] = func(op int) int { return op * op * op }
+	t.Log(m[1](2), m[2](2), m[3](2))
+}
+
+func TestMapForSet(t *testing.T) {
+	mySet := map[int]bool{}
+	mySet[1] = true
+	n := 3
+	if mySet[n] {
+		t.Logf("%d is existing", n)
+	} else {
+		t.Logf("%d is not existing", n)
+	}
+	t.Log(len(mySet))
+	mySet[3] = true
+	t.Log(len(mySet))
+	delete(mySet, 1) // 删除key为1的元素
+	n = 1
+	if mySet[n] {
+		t.Logf("%d is existing", n)
+	} else {
+		t.Logf("%d is not existing", n)
+	}
 }
