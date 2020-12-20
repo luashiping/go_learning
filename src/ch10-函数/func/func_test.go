0198963d66fbc1f2a9cf085632557ba25526d75f
lushiping <lushiping@360.cn>
2020-12-20 15:45:09 +0800

    update ch10-函数

--- src/ch10-函数/func/func_test.go
+++ src/ch10-函数/func/func_test.go
@@ -1,6 +1,7 @@
 package func_test
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 	"testing"
@@ -36,6 +37,7 @@ func TestFn(t *testing.T) {
 	t.Log(ret)
 }
 
+//可变参数，函数的参数数量可变
 func Sum(ops ...int) int {
 	ret := 0
 	for _, op := range ops {
@@ -51,6 +53,56 @@ func TestVarParam(t *testing.T) {
 	ret1 := Sum(1, 2, 3, 4, 5)
 	t.Log(ret1)
 
+}
+
+/*
+命名返回参数
+不止函数的参数可以有变量名称，函数的返回值也可以，也就是说你可以为每个返回值都起一个名字，这个名字可以像参数一样在函数体内使用
+*/
+func sum(a, b int) (sum int, err error) {
+	if a < 0 || b < 0 {
+		return 0, errors.New("a或者b不能是负数")
+	}
+	sum = a + b
+	err = nil
+
+	return
+}
+
+func TestReturnName(t *testing.T) {
+	res, _ := sum(1, 3)
+	fmt.Println(res)
+}
+
+// 闭包
+func colsure() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+func TestClosure(t *testing.T) {
+	cl := colsure()
+
+	fmt.Println(cl())
+	fmt.Println(cl())
+	fmt.Println(cl())
+}
+
+// 不同于函数的方法
+type Age uint
+
+func (age Age) String() {
+	fmt.Println("the age is", age)
+}
+
+func TestMethods(t *testing.T) {
+	// age := Age(26)
+	// age.String()
+	age := Age(25)
+	sm := Age.String
+	sm(age)
 }
 
 func Clear() {
