98971208958a501a9cc8525ff8b905f9cc1617e4
lushiping <lushiping@360.cn>
2022-10-18 13:25:52 +0800

    字符串学习笔记

--- src/ch9-字符串/string/string_test.go
+++ src/ch9-字符串/string/string_test.go
@@ -3,7 +3,30 @@ package string_test
 import (
 	"fmt"
 	"testing"
+	"unicode/utf8"
 )
+
+func TestStringJoin(t *testing.T) {
+	s := "Rob Pike, "
+	s = s + "Robert Griesemer, "
+	s += " Ken Thompson"
+
+	t.Log(s) // Rob Pike, Robert Griesemer, Ken Thompson
+}
+
+func TestDecodRune(t *testing.T) {
+	var buf = []byte{0xE4, 0xB8, 0xAD}
+	r, _ := utf8.DecodeRune(buf)                                                             // 对buf进行utf-8解码
+	fmt.Printf("the unicode charactor after decoding [0xE4, 0xB8, 0xAD] is %s\n", string(r)) // 中
+}
+
+func TestEncodeRune(t *testing.T) {
+	var r rune = 0x4E2D
+	fmt.Printf("the unicode charactor is %c\n", r) // 中
+	buf := make([]byte, 3)
+	_ = utf8.EncodeRune(buf, r)                       // 对rune进行utf-8编码
+	fmt.Printf("utf-8 representation is 0x%X\n", buf) // 0xE4B8AD
+}
 
 func TestString(t *testing.T) {
 	var s string
